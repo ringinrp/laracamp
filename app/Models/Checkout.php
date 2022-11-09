52a0b00c89480e506c7c7480760e3082fbec2f70
Ringin Restu Pati <ringinrp21@gmail.com>
2022-11-09 21:05:00 +0700

    finish migration Camp, Camp_benefit, Checkout

---
+++ app/Models/Checkout.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Checkout extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'card-number',
+        'expired',
+        'cvc',
+        'is_paid'
+    ]
+}
